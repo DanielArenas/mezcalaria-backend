0d3de636efa7b8b0c971db0c0e5d60d01abad196
Daniel Arenas <drawdream1144@gmail.com>
2017-06-27 14:47:52 -0500

    add dowload users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,14 @@ class UsersController < ApplicationController
 
 	def index
 		@users = Usuario.where(rol_id: 2)
+
+		respond_to do |format|
+			format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"usuarios-#{Date.today}.csv\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+		end
 	end
+
 end
