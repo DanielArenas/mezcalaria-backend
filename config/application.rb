66cf46254c2c4bf4563332db27f9bb063383f572
Daniel Arenas <drawdream1144@gmail.com>
2017-06-29 22:42:17 -0500

    update version

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module MezcalariaBackend
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
     # Version app
-    config.VERSION = '0.1.1'
+    config.VERSION = '0.1.2'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
